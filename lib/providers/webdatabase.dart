ca90dbfa73f7f51c2ed3518b0129b9d8323d222b
Jesper Halkjær <gedemagt@gmail.com>
2019-04-12 00:56:23 +0200

    Better import.

--- lib/providers/webdatabase.dart
+++ lib/providers/webdatabase.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:flutter/src/widgets/image.dart';
+import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
